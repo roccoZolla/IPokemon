cc94751ee9229e10fe422b0d22e0735c425555d5
Rocco Piazzolla <roccopiazzo2001@gmail.com>
2023-06-13 18:54:52 +0200

    - eliminate librerie non necessarie

--- IPokemon/FightPage.xaml.cs
+++ IPokemon/FightPage.xaml.cs
@@ -2,21 +2,14 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 using Newtonsoft.Json;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Markup;
 using Windows.UI;
 using System.Threading.Tasks;
 
